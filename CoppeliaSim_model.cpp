9018ffd067678de01b5e41963e766c8f890716af
gjhhust <gjh_hust@foxmail.com>
2021-09-08 18:50:22 +0800

    添加项目文件。

---
+++ CoppeliaSim_model.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include"extApi.h"
+void main()
+{
+
+	using namespace std;
+
+	int Port = 19997;
+
+	int clientID = simxStart("127.0.0.1", Port, 1, 1, 1000, 5);
+
+	if (clientID != -1)
+	{
+		cout << "V-rep connected.";
+		simxStartSimulation(clientID,simx_opmode_oneshot);
+	}
+	else
+	{
+		cout << "V-rep can't be connected.";
+	}
+
+	cin.get();
+	return;
+}
